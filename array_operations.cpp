fb6f9a0dc5601f0b119597ad78117047569fb766
FrasIrfan <leofras0051@gmail.com>
2024-05-04 00:22:17 +0500

    delete operation in array

--- array_operations.cpp
+++ array_operations.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-// #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
-
 using namespace std;
 
-void insertAt(int arr[], int array_size, int index, int element)
+void insertAt(int arr[], int &array_size, int index, int element)
 {
     // Checking for Invalid index
     if (index < 0 || index > array_size)
@@ -14,20 +11,40 @@ void insertAt(int arr[], int array_size, int index, int element)
     }
 
     // Checking if the array is full
-    if (array_size == sizeof(arr) / sizeof(arr[0]))
+    if (array_size == (sizeof(arr) / sizeof(arr[0])))
     {
         cout << "Array is full" << endl;
         return;
     }
 
     // Shifting elements to the right from the index
-    for (int i = array_size - 1; i >= index; i--)
+    for (int i = array_size; i > index; i--)
     {
-        arr[i + 1] = arr[i];
+        arr[i] = arr[i - 1];
     }
 
     arr[index] = element;
-    cout << "\nInserted element at index => " << index << endl;
+    array_size++;
+    cout << "\nInserted element at index => " << index;
+}
+
+void deleteAt(int arr[], int &array_size, int index)
+{
+    // Checking for Invalid index
+    if (index < 0 || index >= array_size)
+    {
+        cout << "Invalid Index" << endl;
+        return;
+    }
+
+    // Shifting elements to the left from the index
+    for (int i = index; i < array_size - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    array_size--;
+    cout << "\nDeleted element at index => " << index << endl;
 }
 
 int main()
@@ -43,12 +60,26 @@ int main()
     {
         cout << arr[i] << " ";
     }
-    insertAt(arr,array_size,index,element);
-    cout << "Array after insertion: ";
-    for (int i = 0; i < array_size ; i++)
-{
-    cout << arr[i] << " ";
-}
+    insertAt(arr, array_size, index, element);
+    cout << "\nArray after insertion: ";
+    for (int i = 0; i < array_size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nArray before deletion: ";
+    for (int i = 0; i < array_size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    index = 2;
+    deleteAt(arr, array_size, index);
+    cout << "Array after deletion: ";
+    for (int i = 0; i < array_size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     cout << endl;
 
     return 0;
