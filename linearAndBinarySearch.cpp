1822f2ec850b026e5e1119efb00690612c41372b
FrasIrfan <leofras0051@gmail.com>
2024-05-06 01:52:42 +0500

    found element using binary search

--- linearAndBinarySearch.cpp
+++ linearAndBinarySearch.cpp
@@ -12,14 +12,43 @@ int LinearSearch(int arr[], int array_size, int element)
     }
     return -1;
 }
+int BinarySearch(int arr[], int array_size, int element)
+{
+    int low = 0;
+    int high = array_size - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == element)
+        {
+            return mid;
+        }
+        else if (arr[mid] < element)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
 
 int main()
 {
-    int arr[] = {2, 4, 6, 8, 10,12,7,9,3,22,5};
-    int array_size = sizeof(arr) / sizeof(arr[0]);
-    int element = 3;
+    // Array for Linear Search
+    //  int arr[] = {2, 4, 6, 8, 10,12,7,9,3,22,5};
+    //  int array_size = sizeof(arr) / sizeof(arr[0]);
+    //  int element = 3;
 
-    int SearchIndex = LinearSearch(arr, array_size, element);
+    // Array for Binary Search
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int array_size = sizeof(arr) / sizeof(arr[0]);
+    int element = 6;
+
+    int SearchIndex = BinarySearch(arr, array_size, element);
     if (SearchIndex == -1)
     {
         cout << "Element not found" << endl;
@@ -28,6 +57,8 @@ int main()
     {
         cout << "Element found at index => " << SearchIndex << endl;
     }
+
+
 
     return 0;
 }
